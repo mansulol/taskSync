7458ede91265c18dc4e5646867b5044ef739285a
Mansour Lo Lo <mansourlol440@gmail.com>
2026-04-13 17:35:58 +0100

    fix path alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,12 +8,12 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@components": path.resolve(__dirname, "./src/components/"),
-      "@utils": path.resolve(__dirname, "./src/utils/"),
-      "@pages": path.resolve(__dirname, "./src/pages/"),
-      "@services": path.resolve(__dirname, "./src/services/"),
-      "@types": path.resolve(__dirname, "./src/types/"),
-      "@": path.resolve(__dirname, "./src/"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@types": path.resolve(__dirname, "./src/types"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
