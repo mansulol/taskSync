16452eb6ceb9bae0ed7e3a4bf2ebbc65e547f852
Mansour Lo Lo <mansourlol440@gmail.com>
2026-04-15 21:30:33 +0100

    tailwind + heroui configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,10 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), 
-  ],
+  plugins: [ react(), tailwindcss()],
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "./src/components"),
